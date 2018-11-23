201ead4e611b90ec4b09d9ea5d0517d5013eed80
EmaSuriano <emanuel.suriano@gmail.com>
2018-11-23 09:47:29 +0100

    Doing some cleanup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/require-default-props': 'off',
+  },
+  parser: 'babel-eslint',
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,97 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import format from 'date-fns/format'
+import { StaticQuery, graphql } from 'gatsby'
+import React, { Component } from 'react'
+import Container from '../components/Container'
+import Layout from '../components/Layout'
+import Month from '../components/Calendar/Month'
+import ModalEvent from '../components/ModalEvent'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+class CalendarPage extends Component {
+  state = {
+    currentDay: new Date(),
+    eventsOfTheDay: [],
+    showModal: false,
+  }
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  hideModal = () => {
+    this.setState({ showModal: false }, () => this.toggleOverflow(false))
+  }
 
-export default IndexPage
+  showModal = (eventsOfTheDay, currentDay) => {
+    this.setState(
+      {
+        currentDay,
+        eventsOfTheDay,
+        showModal: true,
+      },
+      () => this.toggleOverflow(true),
+    )
+  }
+
+  toggleOverflow = active => {
+    // TODO: puedo lograr esto de una manera mejor? Modificar un elemento del DOM
+    // que no solo está fuera de mi componente sino que fuera del root de la app
+    document
+      .querySelector('body')
+      .classList[active ? 'add' : 'remove']('overflow-hidden')
+  }
+
+  render() {
+    const { currentDay, eventsOfTheDay, showModal } = this.state
+
+    return (
+      <Layout>
+        <Container large="large">
+          <div className="fade-in">
+            <StaticQuery
+              query={graphql`
+                {
+                  allGoogleSheetEventosRow {
+                    edges {
+                      node {
+                        id
+                        date: fecha
+                        eventName: nombre
+                        eventLink: link
+                        place: lugar
+                      }
+                    }
+                  }
+                }
+              `}
+              render={data => {
+                const events = data.allGoogleSheetEventosRow.edges.map(
+                  ({ node }) => node,
+                )
+                const monthlyCalendar = {
+                  events,
+                  when: {
+                    month: 'noviembre',
+                    year: '18',
+                  },
+                }
+                console.log(events)
+                return (
+                  <Month
+                    monthlyCalendar={monthlyCalendar}
+                    events={events}
+                    showModal={this.showModal}
+                  />
+                )
+              }}
+            />
+          </div>
+        </Container>
+        {showModal && (
+          <ModalEvent
+            hideModal={this.hideModal}
+            currentDay={currentDay}
+            events={eventsOfTheDay}
+          />
+        )}
+      </Layout>
+    )
+  }
+}
+
+export default CalendarPage
